abb0523ba4053e57833c945abb03acceb034a2ae
kapkande <kap.kande@gmail.com>
2023-07-28 11:25:55 +0300

    init: add getLocal

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-const LOCATION = { center: [27.5667,  53.9], zoom: 12 };
+const LOCATION = {
+    center: [27.5667,53.9000],
+    zoom: 15
+};
+
+
+
+// (async function getLoc() {
+//     const ipObject = await fetch(`https://api.ipify.org/?format=json`);
+//     const ip = await ipObject.json();
+    
+//     let response = await fetch(`https://ipinfo.io/${ip.ip}?token=504d8a3587161a`);
+//     let user = await response?.json();
+//     console.log(user);
+
+//    LOCATION.center = user.loc;
+//    main()
+// })()
+// LOCATION.center = inputText
 
 window.map = null;
+
+
+
 
 main();
 async function main() {
@@ -11,11 +32,11 @@ async function main() {
         YMapControls
     } = ymaps3;
 
-    const {YMapZoomControl} = await ymaps3.import('@yandex/ymaps3-controls@0.0.1');
+    const { YMapZoomControl } = await ymaps3.import('@yandex/ymaps3-controls@0.0.1');
 
-    map = new YMap(document.getElementById('app'), {location: LOCATION}, [
+    map = new YMap(document.getElementById('app'), { location: LOCATION }, [
         new YMapDefaultSchemeLayer(),
-        new YMapControls({position: 'right'}, [
+        new YMapControls({ position: 'right' }, [
             new YMapZoomControl({})
         ]),
     ]);
